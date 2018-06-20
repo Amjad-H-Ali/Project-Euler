9c1a6f695bc0eb81fb2128da61cd60590da76f6a
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-19 23:17:50 -0500

    Function finds sum of all natural numbers that are multiple of 3 or 5 below 1000

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,19 @@
 console.log("Project Euler");
 
-const euler1 = (num) => {
-	let sum = 0;
 
+const euler1 = (num) => {
+	// To add up the numbers that pass the following condition.
+	let sum = 0;
+	// Iterate up to number passed as argument.
 	for (let i = 1; i < num; i ++) {
+		// Check if Number is a multiple of 3 or 5.
 		if (i % 3 === 0 || i % 5 === 0) {
+			// If it is, add it to sum variable.
 			sum += i;
 		}
 	};
-
+	// Return the sum of all number that are multiples of 3 or 5.
 	return sum;
 };
 
-console.log(euler1(10));
+console.log(euler1(1000)); //233168
